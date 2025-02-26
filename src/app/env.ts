109a400fa30ec4493589ce986d18678622c92797
Ankit Barmase <ankitbarmase26@gmail.com>
2025-02-26 17:59:03 +0530

    config done

--- src/app/env.ts
+++ src/app/env.ts
@@ -2,7 +2,7 @@ const env = {
     appwrite:{
         endpoint: String(process.env.NEXT_PUBLIC_APPWRITE_HOST_URL),
         projectId: String(process.env.NEXT_PUBLIC_APPWRITE_PROJJECT_ID),
-        appikey: String(process.env.APPWRITE_API_KEY)
+        apikey: String(process.env.APPWRITE_API_KEY)
     }
 }
 
---
+++ src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-orionflow"
+export const questionCollection = "questions"
+export const answerCollection = "answers"
+export const commentCollection = "comments"
+export const voteCollection = "votes"
+export const questionAttachmentBucket = "question-attachment"
